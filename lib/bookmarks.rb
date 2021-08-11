ee332b07e098bfe9103e2498314a93a61d459d09
NazirShaikh <shain003.212@gmail.com>
2021-08-11 17:02:14 +0100

    replaced exec with exec_params to prevent SQL injection on user form page

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -28,7 +28,9 @@ class Bookmarks
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title;")
+    result = connection.exec_params(
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
     Bookmarks.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
